cb76aec36686bd52535d47e0e2ae86d1100c139b
yul-zhong <m13545022224@163.com>
2022-06-01 15:23:24 +0800

    www

---
+++ main.py
@@ -0,0 +1,7 @@
+from curses.ascii import FF
+import streamlit as st
+
+st.title('our first streamlit app')
+st.subheader('Introducing streamlit in automate everything with python')
+st.write('''this is our first web app''')
+
